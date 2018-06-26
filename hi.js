21448bde9b5065309e9ff8f326287577269ae00e
Cow <kildonan500@gmail.com>
2018-06-26 16:00:27 -0400

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,2 +1,2 @@
-document.write ("This is remote text via xss.js located at xss.rocks");
-alert ("This is remote text via xss.js located at xss.rocks ");
+document.write ("XSS");
+alert ("Red Team");
